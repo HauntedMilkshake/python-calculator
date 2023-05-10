008fe2c0838e7214ed924d420d865fc334e7bf4f
Spas Zahov <spaszahov@gmail.com>
2023-05-10 17:15:32 +0300

    trying to implement graphz for a block scheme

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+#използвмае библиотека за потребителския интерфейс 
 import tkinter as tk
 
 class Calculator:
@@ -5,20 +6,21 @@ class Calculator:
         self.master = master
         master.title("Calculator")
         
-        # Create the calculator display
+        # Създаваме дисплейа на калкулатора
         self.display_var = tk.StringVar()
         display = tk.Entry(master, textvariable=self.display_var, font=("Arial", 18))
         display.grid(row=0, column=0, columnspan=4, padx=5, pady=5, sticky="nsew")
         
-        # Create the calculator buttons
+        # бутоните, които калкулаторът ще има
         buttons = [
             "7", "8", "9", "/",
             "4", "5", "6", "*",
             "1", "2", "3", "-",
-            "0", ".", "=", "+"
+            "0", ".", "=", "+",
+            "c"
         ]
         
-        # Add buttons to the GUI window
+        # добавяне на бутоникте към калкулатора
         row = 1
         col = 0
         for button in buttons:
@@ -28,7 +30,7 @@ class Calculator:
             if col > 3:
                 col = 0
                 row += 1
-
+    #метод за проверка дали бутон е натиснат
     def button_click(self, button):
         if button == "C":
             self.display_var.set("")
@@ -37,6 +39,8 @@ class Calculator:
                 self.display_var.set(str(eval(self.display_var.get())))
             except:
                 self.display_var.set("Error")
+        elif button == "c":
+            self.display_var.set("");
         else:
             self.display_var.set(self.display_var.get() + button)
 
